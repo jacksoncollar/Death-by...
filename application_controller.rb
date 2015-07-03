39a2703627459eeef49b531f94332521cd9fa6cc
eytanrosenman <eytanrosenman@gmail.com>
2015-07-03 17:56:44 +0000

    app control

--- application_controller.rb
+++ application_controller.rb
@@ -5,11 +5,23 @@ Bundler.require
 class MyApp < Sinatra::Base
     get '/' do
       erb :index
-  end
+    end
   
   post '/' do
     Eytan = Death.new(params)
     e_answers = Eytan.quiz_response
-    @highest_key = Eytan.highest_key
+    @highest_key = Eytan.highest_key 
+    if @highest_key == "lawnmower"
+      erb :lawnmower
+    elsif @highest_key == "toilet"
+      erb :toilet
+    elsif @highest_key == "beard"
+      erb :beard
+    elsif @highest_key == "combustion"
+      erb :sc
+    elsif @highest_key == "volcano"
+      erb :volcano
+    end
+    
   end
 end
